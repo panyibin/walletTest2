d8a668f90baf4cc848b814697b036c71836142fe
PanYibin <>
2018-03-14 09:05:55 +0800

    initial UI

--- App.js
+++ App.js
@@ -9,7 +9,11 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  TextInput,
+  Button,
+  Alert,
+  TouchableOpacity
 } from 'react-native';
 
 const instructions = Platform.select({
@@ -19,40 +23,99 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+const onButtonPress = () => {
+   Alert.alert('generate seed');
+};
+
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native! ~~~
+          Create a wallet
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit App.js
+          It's your first time using our mobile wallet, so you'll need to either create a new wallet or load an existing one from a seed.
         </Text>
-        <Text style={styles.instructions}>
-          {instructions}
+        <Text style={styles.subTitleWallet}>
+        Name your wallet
         </Text>
+        <TextInput style={styles.textInputWallet}></TextInput>
+        <Text style={styles.subTitleSeed}>
+        Seed
+        </Text>
+        <TextInput style={styles.textInputSeed}></TextInput>
+        <TouchableOpacity style={styles.seedGenerateButton} onPress={onButtonPress}>
+        <Text style={styles.seedGenerateButtonInside}>Generate seed</Text>
+        </TouchableOpacity>
       </View>
     );
   }
 }
 
+const commonLeftMargin = 35;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: 'flex-start',
+    //alignItems: 'center',
+    backgroundColor: 'blue',
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    marginTop: 50,
+    color:'white'
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
+    textAlign: 'left',
+    color: 'white',
+    marginTop: 15,
     marginBottom: 5,
+    marginLeft:35,
+    marginRight:35,
   },
+  subTitleWallet: {
+    textAlign: 'left',
+    fontSize:15,
+    color: 'white',
+    marginTop:50,
+    marginLeft:35
+  },
+  subTitleSeed: {
+    textAlign: 'left',
+    fontSize:15,
+    color: 'white',
+    marginTop:50,
+    marginLeft:35
+  },
+  textInputWallet: {
+    fontSize:15,
+    backgroundColor:'white',
+    marginLeft:35,
+    marginRight:35,
+    marginTop:10
+  },
+  textInputSeed: {
+    fontSize:15,
+    backgroundColor:'white',
+    marginLeft:35,
+    marginRight:35,
+    marginTop:10
+  },
+  seedGenerateButton: {
+    // backgroundColor:'green',
+    marginLeft:35,
+    marginRight:35,
+    marginTop:10,
+  },
+  seedGenerateButtonInside: {
+    color:'white',
+    // marginRight:35,
+    textAlign:'right',
+    // backgroundColor:'red'
+  },
+
 });
